b3bb979f0b708378624c2b350b338e3363a0302a
Jakub Tkadlecek <jakub@mewssystems.com>
2018-09-30 20:46:56 +0200

    fix condition

--- app/src/index.js
+++ app/src/index.js
@@ -15,7 +15,7 @@ const middlewares = [
     websocketMiddleware
 ];
 // TODO enable condition for final build
-if (true && process.env.NODE_ENV === `development`) {
+if (true || process.env.NODE_ENV === `development`) {
     middlewares.push(logger);
 }
 
