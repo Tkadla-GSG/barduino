47aaefac744913bab87030777942b483192682e9
Jakub Tkadlecek <jakub@mewssystems.com>
2018-10-01 23:04:25 +0200

    ESP => Arduino comm protocol

---
+++ arduino/controller/controller.ino
@@ -0,0 +1,69 @@
+/*
+  Stepper Motor Demonstration 4
+  Stepper-Demo4.ino
+  Demonstrates NEMA 17 Bipolar Stepper with A4988 Driver
+ 
+  DroneBot Workshop 2018
+  https://dronebotworkshop.com
+*/
+ 
+// Define Constants
+ 
+// Connections to A4988
+const int dirPin = 2;  // Direction
+const int stepPin = 3; // Step
+ 
+// Motor steps per rotation
+const int STEPS_PER_REV = 200;
+
+// for incoming serial data
+int incomingByte = 0;
+ 
+void setup() {
+  // setup hardware
+  Serial.begin(115200);
+
+  // Setup the pins as Outputs
+  pinMode(stepPin,OUTPUT); 
+  pinMode(dirPin,OUTPUT);
+}
+void loop() {
+
+  // send data only when you receive data:
+  if (Serial.available() > 0) {
+      // read the incoming byte:
+      incomingByte = Serial.read();
+
+      // say what you got:
+      Serial.print("I received: ");
+      Serial.println(incomingByte, DEC);
+  }
+  
+  // Set motor direction clockwise
+  //digitalWrite(dirPin,HIGH); 
+  
+  // Spin motor one rotation slowly
+  /*for(int x = 0; x < STEPS_PER_REV; x++) {
+    digitalWrite(stepPin,HIGH); 
+    delayMicroseconds(2000); 
+    digitalWrite(stepPin,LOW); 
+    delayMicroseconds(2000); 
+  }
+  
+  // Pause for one second
+  delay(1000); 
+  */
+  // Set motor direction counterclockwise
+  digitalWrite(dirPin,LOW);
+  
+  // Spin motor two rotations quickly
+  for(int x = 0; x < (STEPS_PER_REV * 4); x++) {
+    digitalWrite(stepPin,HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(stepPin,LOW);
+    delayMicroseconds(1000);
+  }
+  
+  // Pause for one second
+  delay(1000);
+}
--- esp8266/web-server/web-server.ino
+++ esp8266/web-server/web-server.ino
@@ -6,18 +6,19 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
+#include <WebSocketsServer.h>
 
-#include <WebSocketsClient.h>
 #include <ArduinoJson.h>
 
 #define WIFI_SSID           "Ragnarok"
 #define WIFI_PASSWORD       "maninyWIFIPassw0rd"
 #define SERVER_PORT         80
+#define WEBSOCKET_PORT      81
 
-WebSocketsClient webSocket;
-
-// WebServer local_ip:80
+// HTTP Server http://local_ip:80
 ESP8266WebServer server(SERVER_PORT);
+// Websocket Server ws://local_ip:81
+WebSocketsServer webSocketServer(WEBSOCKET_PORT);
 
 const char MAIN_page[] PROGMEM = R"=====(
 <!doctype html>
@@ -41,67 +42,69 @@ const char MAIN_page[] PROGMEM = R"=====(
 
 void handleRoot() {
   String page = MAIN_page;
+  String ip = WiFi.localIP().toString() + ":" + WEBSOCKET_PORT;
+  
   // cache bust
   page.replace("_version_", String(random(999999999)));
-  page.replace("_ip_", WiFi.localIP().toString());
+  page.replace("_ip_", ip);
+  
   server.send(200, "text/html", page);
 }
 
-/*
-void sendMessage(String message) {
-  Serial.println(message);
-  
-  DynamicJsonBuffer jsonBuffer;
-  JsonObject& root = jsonBuffer.createObject();
-  root["type"] = "message";
-  root["id"] = nextCmdId++;
-  root["text"] = message;
-  root["channel"] = DEV_CHANNEL_ID;
+void sendStatus() {
   String json;
-  root.printTo(json);
-  webSocket.sendTXT(json);
+  DynamicJsonBuffer jsonBuffer;
+  JsonObject& action = jsonBuffer.createObject();
+  JsonObject& payload = action.createNestedObject("payload");
+  
+  action["type"] = "SERVER_STATUS";
+  payload["queueLength"] = 0;
+  payload["brewing"] = "false";
+  
+  action.printTo(json);
+  webSocketServer.broadcastTXT(json);
 }
-*/
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t len) {
+  switch (type) {
+    case WStype_DISCONNECTED:
+      break;
+    
+    case WStype_CONNECTED:
+      sendStatus();
+      break;
+
+    case WStype_TEXT:
+      const size_t bufferSize = JSON_OBJECT_SIZE(1) + 20;
+      DynamicJsonBuffer jsonBuffer(bufferSize);
+      JsonObject& root = jsonBuffer.parseObject(payload);
+      const char* beverageId = root["beverageId"];
+
+      // send command to controller
+      Serial.write((const uint8_t*)beverageId, 1);
+      break;
+  }
+}
 
 void setup() {
   // setup hardware
   Serial.begin(115200);
   randomSeed(analogRead(0));
-  Serial.println();
 
-  Serial.printf("Connecting to %s\n", WIFI_SSID);
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);     //Connect to your WiFi router
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   
-  Serial.println("");
-  Serial.print("Connected, IP address: ");
-  Serial.println(WiFi.localIP());
-  
-  Serial.println("Starting server");
   server.on("/", handleRoot);
   server.begin();
+
+  webSocketServer.begin();                          
+  webSocketServer.onEvent(webSocketEvent);          
 }
 
 void loop() {
   server.handleClient();
-  webSocket.loop();
-}
-
-void webSocketEvent(WStype_t type, uint8_t *payload, size_t len) {
-  switch (type) {
-    case WStype_DISCONNECTED:
-      Serial.printf("[WebSocket] Disconnected\n");
-      break;
-    
-    case WStype_CONNECTED:
-      Serial.printf("[WebSocket] Connected to: %s\n", payload);
-      break;
-
-    case WStype_TEXT:
-      Serial.printf("[WebSocket] Message: %s\n", payload);
-      break;
-  }
+  webSocketServer.loop();
 }
