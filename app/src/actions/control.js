5735d1359f613a3e7228b1a68ed068ede6b3a909
Jakub Tkadlecek <jakub@mewssystems.com>
2018-10-08 23:35:29 +0200

    simplified commands

--- app/src/actions/control.js
+++ app/src/actions/control.js
@@ -1,7 +1,7 @@
 import { createWebsocketSendAction } from '../util';
 
-export const motorOff = id => dispatch => dispatch(createWebsocketSendAction('motor_off_' + id));
+export const motorOff = id => dispatch => dispatch(createWebsocketSendAction('off_' + id));
 
-export const motorLoad = id => dispatch => dispatch(createWebsocketSendAction('motor_on_load_' + id));
+export const motorLoad = id => dispatch => dispatch(createWebsocketSendAction('load_' + id));
 
-export const motorUnload = id => dispatch => dispatch(createWebsocketSendAction('motor_on_unload_' + id));
+export const motorUnload = id => dispatch => dispatch(createWebsocketSendAction('unload_' + id));
